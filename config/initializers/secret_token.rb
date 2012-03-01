080adf93282f5eaf9a160cfd3abc73647b7511b8
Zhang Shaoyu <zsy056@gmail.com>
2012-03-01 21:44:51 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zim::Application.config.secret_token = '123ee665fc088d3062cb9cf5e32303e9a92237b20ae82415600d113a0ce00a6f9eb7fef85c5122c1cd18f28d5835d4c4754a1a1d53f130bb8e945ae80bb94495'
